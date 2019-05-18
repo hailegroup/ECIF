0108b71a79eec0a991549b108b18d12bf51f8322
aplymill7 <47641688+aplymill7@users.noreply.github.com>
2019-05-18 16:58:17 -0500

    Update Test_Fitting.py

--- main/R_RQ_RQ/Test_Fitting.py
+++ main/R_RQ_RQ/Test_Fitting.py
@@ -34,16 +34,22 @@ params=[0.4, 0.8, 1*10**-2, 1, 0.9, 1*10**-2, 1]
 #Fit the data
 fit_result = fit.custom_fitting(F, TotArr, params)
 Fitted_variables = fit_result.x
+generation = fit.model(Fitted_variables, FArr)
+First_Fit_R = generation.real
+First_Fit_i = generation.imag
 
 #Obtain the residuals
 residuals = fit.res_vec( Fitted_variables, FArr, TotArr)
 
 #Bootstrap to get error and generate final model
-boot_params = boot.strap(residuals, FArr, TotArr, params)
+boot_params = boot.strap(residuals, FArr, TotArr, Fitted_variables)
 boot_generation = fit.model(boot_params, FArr)
 Real_Boot_Fit = boot_generation.real
 Imag_Boot_Fit = boot_generation.imag
 
 #Nyquist Plot
-plt.plot(Real_Boot_Fit, -Imag_Boot_Fit)
-plt.plot(R, Im)
+plt.plot(Real_Boot_Fit, -Imag_Boot_Fit, label = 'Boot Fit')
+plt.plot(R, Im, label = 'Measured')
+plt.plot(First_Fit_R, -First_Fit_i, label = 'First Fit')
+plt.legend(loc='upper left')
+
