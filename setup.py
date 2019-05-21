176e393b3d31f8e58a0ea2cc254974d6f6c38c35
aplymill7 <47641688+aplymill7@users.noreply.github.com>
2019-05-21 14:58:51 -0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+names = [
+]
+
+#entry_points = {
+#    "console_scripts": [
+#        f"{name}= xpdan.startup.{name}:run_main" for name in names
+#    ]
+#}
+
+setup(
+    name="Impedance Fitting",
+    version='0.0.1',
+    packages=find_packages(),
+    description="python API for impedance fitting",
+    zip_safe=False,
+    package_data={"pytentiostat": ["config/*"]},
+    include_package_data=True,
+    url="http:/github.com/aplymill7/Impedance-Fitting",
+    entry_points=entry_points,
+)
