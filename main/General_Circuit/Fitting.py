210bef1d6f9dc866f3c1b2df84b623a79559b3d5
aplymill7 <47641688+aplymill7@users.noreply.github.com>
2019-05-24 16:49:55 -0500

    Update Fitting.py

--- main/General_Circuit/Fitting.py
+++ main/General_Circuit/Fitting.py
@@ -6,15 +6,10 @@ import config
 #Get the type of circuit from the config file
 modelname = config.Circuit_Type
 
-# define impedance model
-def model(parameters, x, modelname):
-    result, lower_bound, upper_bound = cir.Z(parameters, x, modelname)
-
 # define residual function for fitting
 def res_vec(parameters, x, y):
-    y_cal, lb, ub = model(parameters, x, modelname)
-    z = y-y_cal
-    modulus = numpy.sqrt(y_cal.real ** 2 + y_cal.imag ** 2)
+    z = y-result
+    modulus = numpy.sqrt(result.real ** 2 + result.imag ** 2)
     a = z.real/modulus
     b = z.imag/modulus
     z = a.tolist()+b.tolist()
@@ -23,10 +18,9 @@ def res_vec(parameters, x, y):
 
 # least-square fitting
 def custom_fitting(freq, data, parameters):
-    blank, lb, ub = model(parameters, freq, modelname)
+    result, lb, ub = cir.Z(parameters, freq, modelname)
     x_data = numpy.array(freq)
     y_data = numpy.array(data)
     parameters = numpy.array(parameters)
     fitting_result = least_squares(res_vec,parameters,bounds=(lb,ub), args=(x_data, y_data))
     return fitting_result
-
