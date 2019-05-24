5faf5a423ec3209312a2c0c6e13fb7b193c85f83
aplymill7 <47641688+aplymill7@users.noreply.github.com>
2019-05-24 13:49:57 -0500

    Update Circuits.py

--- main/General_Circuit/Circuits.py
+++ main/General_Circuit/Circuits.py
@@ -1,7 +1,10 @@
+import numpy
 
 def Z(parameters, x, modelname):
-    if (modelname == 'RRQRQ')
+    if (modelname == 'RRQRQ'):
+        x = numpy.array(x)
         a, b, c, d, e, f, g, = parameters
-        return a + 1 / (1 / b + c * (1j * 2 * 3.14159 * x) ** d) + 1 / (1 / e + f * (1j * 2 * 3.14159 * x) ** g), [0.0, 0.0, 0.0, 0.0, 0.0 , 0.0 , 0.0], [numpy.inf, numpy.inf, numpy.inf, 1.0, numpy.inf, numpy.inf, 1.0]
-    else
+        result = a+1/(1/b+c*(1j*2*3.14159*x)**d)+1/(1/e+f*(1j*2*3.14159*x)**g)
+        return result, [0.0, 0.0, 0.0, 0.0, 0.0 , 0.0 , 0.0], [numpy.inf, numpy.inf, numpy.inf, 1.0, numpy.inf, numpy.inf, 1.0]
+    else:
         print('Invalid modelname provided')
