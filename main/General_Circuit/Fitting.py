deca461b293593a61dcee7f59e96f97c7517c1b6
aplymill7 <47641688+aplymill7@users.noreply.github.com>
2019-05-24 12:49:56 -0500

    Update and rename Fitting_R_RQ_RQ.py to Fitting.py

--- main/General_Circuit/Fitting.py
+++ main/General_Circuit/Fitting.py
@@ -6,14 +6,9 @@ import Circuits as cir
 def model(parameters, x, modelname):
     result, bound = Circuits.Z(parameters, x, modelname)
 
-# define lower and upper bound for parameters
-def bound():
-    return [0.0, 0.0, 0.0, 0.0, 0.0 , 0.0 , 0.0], [numpy.inf, numpy.inf, numpy.inf, 1.0, numpy.inf, numpy.inf, 1.0]
-
-
 # define residual function for fitting
 def res_vec(parameters, x, y):
-    y_cal = model(parameters, x, modelname)
+    y_cal, bounds = model(parameters, x, modelname)
     z = y-y_cal
     modulus = numpy.sqrt(y_cal.real ** 2 + y_cal.imag ** 2)
     a = z.real/modulus
@@ -27,5 +22,5 @@ def custom_fitting(freq, data, parameters):
     x_data = numpy.array(freq)
     y_data = numpy.array(data)
     parameters = numpy.array(parameters)
-    fitting_result = least_squares(res_vec,parameters,bounds=(bound()), args=(x_data, y_data))
+    fitting_result = least_squares(res_vec,parameters,bounds, args=(x_data, y_data))
     return fitting_result
