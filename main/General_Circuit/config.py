b221c53f81120d77695f7ad4c16e2b74ddd3876c
aplymill7 <47641688+aplymill7@users.noreply.github.com>
2019-05-24 13:50:31 -0500

    Update config.py

--- main/General_Circuit/config.py
+++ main/General_Circuit/config.py
@@ -2,4 +2,4 @@
 
 Circuit_Type = 'RRQRQ' #e.g. 'RC'
 
-Initial paramters = [0.4, 0.4, 1*10**-2, 1, 0.4, 1*10**-2, 1] #e. g. [a, b, c, d, e]
+Initial_Parameters = [0.4, 0.4, 1*10**-2, 1, 0.4, 1*10**-2, 1] #e. g. [a, b, c, d, e]
