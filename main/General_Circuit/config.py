c06894b6da06c9343914d61c18557843b427b6e6
aplymill7 <47641688+aplymill7@users.noreply.github.com>
2019-05-29 10:40:48 -0500

    added variable for filename

--- main/General_Circuit/config.py
+++ main/General_Circuit/config.py
@@ -3,3 +3,5 @@
 Circuit_Type = 'RRQRQ' #e.g. 'RC'
 
 Initial_Parameters = [0.4, 0.4, 1*10**-2, 1, 0.4, 1*10**-2, 1] #e. g. [a, b, c, d, e]
+
+Filename = 'test.csv' #Should be three columned csv with that contains frequency, real, and imaginary impedance respectively
