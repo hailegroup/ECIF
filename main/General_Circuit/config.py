3c51e91238484a88cf93804ede4ab5c9c27cc04a
aplymill7 <47641688+aplymill7@users.noreply.github.com>
2019-05-29 12:46:25 -0500

    Adding bounds to the config file

--- main/General_Circuit/config.py
+++ main/General_Circuit/config.py
@@ -4,4 +4,8 @@ Circuit_Type = 'RRQRQ' #e.g. 'RC'
 
 Initial_Parameters = [0.4, 0.4, 1*10**-2, 1, 0.4, 1*10**-2, 1] #e. g. [a, b, c, d, e]
 
+Parameter_Bounds = 
+
 Filename = 'test.csv' #Should be three columned csv with that contains frequency, real, and imaginary impedance respectively
+
+Frequency_Domain = [0, 1000000] #Lower bound and upper bound of frequency range to consider in Hz
