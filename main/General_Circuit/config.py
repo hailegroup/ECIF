ae4aebe1a9ec02bd74291d84416f5bafd0edf8c3
aplymill7 <47641688+aplymill7@users.noreply.github.com>
2019-05-29 12:47:40 -0500

    Update config.py

--- main/General_Circuit/config.py
+++ main/General_Circuit/config.py
@@ -4,7 +4,9 @@ Circuit_Type = 'RRQRQ' #e.g. 'RC'
 
 Initial_Parameters = [0.4, 0.4, 1*10**-2, 1, 0.4, 1*10**-2, 1] #e. g. [a, b, c, d, e]
 
-Parameter_Bounds = 
+Parameter_Lower_Bounds = [0.0, 0.0, 0.0, 0.0, 0.0 , 0.0 , 0.0]
+
+Parameter_Upper_Bounds = []
 
 Filename = 'test.csv' #Should be three columned csv with that contains frequency, real, and imaginary impedance respectively
 
