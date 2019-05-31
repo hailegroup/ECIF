adc4b1fde5e36f19c8918e1687509ba4f405a19a
aplymill7 <47641688+aplymill7@users.noreply.github.com>
2019-05-31 15:28:18 -0500

    Update to incorporate variable parameter names

--- main/General_Circuit/Circuits.py
+++ main/General_Circuit/Circuits.py
@@ -10,29 +10,29 @@ def Z(parameters, x, modelname):
     if (modelname == 'R'):
         a = parameters
         result = a
-        return result, [0.0], [numpy.inf]
+        return result, [0.0], [numpy.inf], ['R0']
     
-     elif (modelname == 'RRC'):
+    elif (modelname == 'RRC'):
         a, b, c = parameters
         result = a +1/(1/b+c*(1j*2*3.14159*x))
-        return result, [0.0, 0.0, 0.0], [numpy.inf, numpy.inf, numpy.inf]   
+        return result, [0.0, 0.0, 0.0], [numpy.inf, numpy.inf, numpy.inf], ['R0', 'R1', 'C1']
     
     elif (modelname == 'RRQ'):
         a, b, c, d = parameters
         result = a +1/(1/b+c*(1j*2*3.14159*x)**d)
-        return result, [0.0, 0.0, 0.0, 0.0], [numpy.inf, numpy.inf, numpy.inf, 1.0]
+        return result, [0.0, 0.0, 0.0, 0.0], [numpy.inf, numpy.inf, numpy.inf, 1.0], ['R0', 'R1', 'Q1', 'Alpha1']
  
     elif (modelname == 'RRCRC'):
         
         a, b, c, d, e = parameters
         result = a+1/(1/b+c*(1j*2*3.14159*x))+1/(1/d+e*(1j*2*3.14159*x))
-        return result, [0.0, 0.0, 0.0, 0.0, 0.0], [numpy.inf, numpy.inf, numpy.inf, numpy.inf, numpy.inf]
+        return result, [0.0, 0.0, 0.0, 0.0, 0.0], [numpy.inf, numpy.inf, numpy.inf, numpy.inf, numpy.inf], ['R0', 'R1', 'C1', 'R2', 'C2']
 
     elif (modelname == 'RRQRQ'):
         
         a, b, c, d, e, f, g = parameters
         result = a+1/(1/b+c*(1j*2*3.14159*x)**d)+1/(1/e+f*(1j*2*3.14159*x)**g)
-        return result, [0.0, 0.0, 0.0, 0.0, 0.0 , 0.0 , 0.0], [numpy.inf, numpy.inf, numpy.inf, 1.0, numpy.inf, numpy.inf, 1.0]
+        return result, [0.0, 0.0, 0.0, 0.0, 0.0 , 0.0 , 0.0], [numpy.inf, numpy.inf, numpy.inf, 1.0, numpy.inf, numpy.inf, 1.0], ['R0', 'R1', 'Q1', 'Alpha1', 'R2', 'Q2', 'Alpha2']
     
     else:
         print('Invalid modelname provided')
